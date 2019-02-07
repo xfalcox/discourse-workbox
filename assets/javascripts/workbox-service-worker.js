7a856429d9d2c92d4b7dd7e6eaa1e2ceee0475fa
Rafael dos Santos Silva <xfalcox@gmail.com>
2019-02-07 17:07:44 -0200

    More caches

--- assets/javascripts/workbox-service-worker.js
+++ assets/javascripts/workbox-service-worker.js
@@ -1,6 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
+  /.+\/(assets|stylesheets|svg\-sprite|javascripts|theme-javascript)\/.+/,
   workbox.strategies.staleWhileRevalidate()
 );
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
