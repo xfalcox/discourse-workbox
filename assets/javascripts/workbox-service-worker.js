734e63e82dcdf52f1b5f24149a62992caa4a7d27
Rafael dos Santos Silva <xfalcox@gmail.com>
2019-05-03 17:04:58 -0300

    Match all GETs

--- assets/javascripts/workbox-service-worker.js
+++ assets/javascripts/workbox-service-worker.js
@@ -1,20 +1,6 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
-  /.+\/(assets|stylesheets|svg\-sprite|javascripts|theme-javascript)\/.+/,
+  /.*?/,
   workbox.strategies.staleWhileRevalidate()
 );
-
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  })
-);
-
