810b3b46be9e4ec1e9bc605ef89f3ffb4ba03ce8
Rafael dos Santos Silva <xfalcox@gmail.com>
2019-05-21 16:34:07 -0300

    Move to cache as only the fallback

--- assets/javascripts/workbox-service-worker.js
+++ assets/javascripts/workbox-service-worker.js
@@ -2,5 +2,5 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 workbox.routing.registerRoute(
   /.*?/,
-  workbox.strategies.staleWhileRevalidate()
+  workbox.strategies.NetworkFirst()
 );
